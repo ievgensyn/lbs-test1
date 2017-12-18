e22bfa5afe964ae265327d3f773a4f6310b7b566
Ievgen <yevgens@web.de>
2017-12-18 22:51:48 +0200

    compute duration of days in the model 'session'

--- addons/test/models/models.py
+++ addons/test/models/models.py
@@ -15,18 +15,31 @@ from datetime import datetime, timedelta
 #     def _value_pc(self):
 #         self.value2 = float(self.value) / 100
 
-
+# model test
 class Test(models.Model):
     _name = 'lbs.test'
 
     name = fields.Char(string="Title", required=True)
-    description = fields.Text()
+    purpose = fields.Text()
+    tester = fields.Many2one('res.partner', ondelete='set null', string="Tester")
 
 
+# model test session:
 class Session(models.Model):
     _name = 'lbs.session'
 
-    name = fields.Char(required=True)
-    start_date = fields.Date()
+    test = fields.Many2one('lbs.test', ondelete='set null', string="Test")
+    start_date = fields.Date(default=fields.Date.today)
     end_date = fields.Date()
-    duration = fields.Float(help="Duration in days", digits=(6, 2))
+    duration = fields.Char(help="Duration in day", compute='_compute_duration')
+
+    @api.depends('start_date', 'end_date')
+    def _compute_duration(self):
+        for r in self:
+            if not (r.start_date and r.end_date):
+                r.duration = r.duration
+                continue
+            start_date = fields.Datetime.from_string(r.start_date)
+            end_date = fields.Datetime.from_string(r.end_date)
+            r.duration = (end_date - start_date)
+
